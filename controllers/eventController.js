eb539efedd1b3a202741bc489a0c3e1f98d3a88d
rajthanusan <rajthanusan08@gmail.com>
2024-12-19 11:51:05 +0530

    errors removed

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,16 +2,6 @@ const mongoose = require('mongoose');
 const Event = require('../models/Event');
 const fs = require('fs');
 const path = require('path');
-require('dotenv').config(); 
-const cloudinary = require('cloudinary').v2; 
-const { upload } = require('../config/cloudinary'); 
-
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 exports.getEvents = async (req, res) => {
   try {
@@ -52,28 +42,22 @@ exports.addEvent = async (req, res) => {
 
 exports.updateEvent = async (req, res) => {
   const { id } = req.params;
-  const { eventname, date, time, location, active } = req.body;
+  const { eventname, date, time, location } = req.body;
 
   try {
-    const updateData = { eventname, date, time, location, active: active === 'true' };
+    const updateData = { eventname, date, time, location };
 
-    
     if (req.file) {
-      console.log('File upload detected:', req.file);
-
-      
       const uploadResult = await cloudinary.uploader.upload(req.file.path);
-      updateData.image = uploadResult.secure_url; 
+      updateData.image = uploadResult.secure_url;
 
-      
       const oldEvent = await Event.findById(id);
       if (oldEvent && oldEvent.image) {
-        const publicId = oldEvent.image.split('/').pop().split('.')[0]; 
-        await cloudinary.uploader.destroy(publicId); 
+        const publicId = oldEvent.image.split('/').pop().split('.')[0];
+        await cloudinary.uploader.destroy(publicId);
       }
     }
 
-    
     const updatedEvent = await Event.findByIdAndUpdate(id, updateData, { new: true });
 
     if (!updatedEvent) {
@@ -82,7 +66,6 @@ exports.updateEvent = async (req, res) => {
 
     res.status(200).json(updatedEvent);
   } catch (error) {
-    console.error('Error updating event:', error.message);
     res.status(500).json({ error: 'Error updating event', details: error.message });
   }
 };
